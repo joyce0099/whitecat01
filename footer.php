149b5effe9ea2942a2141c4e27a5d93f5dddcaf2
joyce0099 <30658360+joyce0099@users.noreply.github.com>
2017-08-03 15:02:35 +1000

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,31 @@
+<div class = "footer">
+        <div class = "footlabel">
+                <a href = "">
+                        <span>
+                                <img src = "./img/ins.png" width = "50" height = "50" alt="">
+                        </span>
+                </a>
+                <a href = "">
+                        <span>
+                                <img src = "./img/fb.jpg" width = "50" height = "50" alt="">
+                        </span>
+                </a>
+                <a href = "">
+                        <span>
+                                <img src = "./img/tw.png" width = "50" height = "50" alt="">
+                        </span>
+                </a>
+        </div>
+
+        <div class = "footwords">
+                <a href = "">ABOUT</a>
+                <a href = "">FAQ</a>
+                <a href = "">TERMS</a>
+                <a href = "">PRIVACY POLICY</a>
+                <a href = "">CONTACT</a>
+        </div>  
+        <div class = "cprt">
+                <span>@Copyright Lynn.s</span>
+                
+        </div>
+</div>
